45928be9b8891ef573da60c956ca0bc275df62d5
julienhouyet <julien.houyet@gmail.com>
2024-06-11 09:53:38 +0200

    feat: add Bureaucrat constructor & destructor

--- ex00/include/BureauCrat.hpp
+++ ex00/include/BureauCrat.hpp
@@ -6,7 +6,29 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 09:20:47 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/11 09:20:47 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/11 09:52:37 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef __BUREAUCRAT_H__
+#define __BUREAUCRAT_H__
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+	public:
+		Bureaucrat( void );
+		Bureaucrat( std::string name, int grade );
+		Bureaucrat( const Bureaucrat& copy );
+		Bureaucrat& operator=( const Bureaucrat& copy );
+		~Bureaucrat( void );
+
+	private:
+		const std::string 	_name;
+		int 				_grade;
+};
+
+#endif
--- ex00/src/BureauCrat.cpp
+++ ex00/src/BureauCrat.cpp
@@ -6,7 +6,37 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 09:20:55 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/11 09:20:56 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/11 09:47:09 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( void ) : _name("Unknow"), _grade(150)
+{
+
+}
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name), _grade(grade)
+{
+	
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat& copy )
+{
+	*this = copy;
+}
+
+Bureaucrat& Bureaucrat::operator=( const Bureaucrat& copy )
+{
+	if (this != &copy)
+	{
+		this->_grade = copy._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat( void )
+{
+	
+}
