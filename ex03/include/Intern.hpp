14c4481820cb30e86e45ec0bc6975d988c27a1cc
Julien Houyet <julien.houyet@gmail.com>
2024-06-12 18:14:12 +0200

    feat: init Intern class

--- ex03/include/Intern.hpp
+++ ex03/include/Intern.hpp
@@ -6,7 +6,27 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:13:10 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/12 17:13:10 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/12 18:01:01 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef __INTERN_H__
+#define __INTERN_H__
+
+#include "AForm.hpp"
+
+// ************************************************************************** //
+//                               Intern Class                                 //
+// ************************************************************************** //
+
+class Intern
+{
+	public:
+		Intern( void );
+		Intern( const Intern& copy );
+		Intern& operator=( const Intern& copy );
+		~Intern( void );
+};
+
+#endif
--- ex03/src/Intern.cpp
+++ ex03/src/Intern.cpp
@@ -6,7 +6,34 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:13:15 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/12 17:13:15 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/12 18:11:09 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+
+// ************************************************************************** //
+//                            Bureaucrat Class                                //
+// ************************************************************************** //
+
+Intern::Intern( void )
+{
+	
+}
+
+Intern::Intern( const Intern& copy )
+{
+	(void)copy;
+	*this = copy;
+}
+
+Intern& Intern::operator=( const Intern& copy )
+{
+	(void)copy;
+	return *this;
+}
+
+Intern::~Intern( void )
+{
+	
+}
