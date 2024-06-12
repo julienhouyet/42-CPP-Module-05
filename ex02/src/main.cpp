f639a2f1c8e9688e3934b411b77c6a415e5eba02
Julien Houyet <julien.houyet@gmail.com>
2024-06-12 15:20:39 +0200

    feat: add try / catch for tests

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 09:21:28 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/12 13:38:22 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/06/12 15:19:18 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@
 
 int	main()
 {
-	Bureaucrat julien("Julien", 1);
-	
-	ShrubberyCreationForm form("Lucas");
-
-	std::cout << form << std::endl;
-
-	std::cout << " " << std::endl;
-
-	julien.signForm(form);
-
-	std::cout << " " << std::endl;
-
-	std::cout << form << std::endl;
+	try {
+		Bureaucrat julien("Julien", 1);
+		ShrubberyCreationForm form("Lucas");
+		std::cout << form << std::endl;
+		std::cout << " " << std::endl;
+		julien.signForm(form);
+		std::cout << " " << std::endl;
+		std::cout << form << std::endl;
+	} catch (const Bureaucrat::GradeTooHighException& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	} catch (const Bureaucrat::GradeTooLowException& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
 
 	return 0;
 }
